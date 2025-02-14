0ceefa5f4afd4bd48be05c1200dd41f3974213b5
DuongPham12 <kingbeeg123@gmail.com>
2025-02-14 17:06:01 +0700

    update

--- GetImagesFromDatabase/App_Start/RouteConfig.cs
+++ GetImagesFromDatabase/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace GetImagesFromDatabase
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "XemAnhSever", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index1", id = UrlParameter.Optional }
             );
         }
     }
